b3f69ac8a687bbdef2fe7c2541fb3dcdfc1e758c
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-06-28 15:19:55 +0300

    made changes

--- server/config.py
+++ server/config.py
@@ -1,7 +1,6 @@
 # Standard library imports
 
 # Remote library imports
-from urllib.parse import urlparse
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -10,7 +9,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-import os
 
 # Local imports
 
@@ -18,13 +16,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 api=Api(app)
-db_url = os.environ.get('DATABASE_URL')
-if db_url:
-    if db_url.startswith('postgres://'):
-        db_url = db_url.replace('postgres://', 'postgresql://', 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bookuser:bookpass@localhost:5432/book_review_db'  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bookuser:xpQBmD6YwJT29rA7CrhAzO301aCKpOHQ@dpg-d1ftpdnfte5s73fvco4g-a/book_review_db_cuwe'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
 app.json.compact = False
@@ -43,7 +35,7 @@ migrate = Migrate(app, db)
 db.init_app(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY', 'fallback-secret-for-dev-only')
+app.config["JWT_SECRET_KEY"] = "super-secret-key"
 
 # Instantiate REST API
 
